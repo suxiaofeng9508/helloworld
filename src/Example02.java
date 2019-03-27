a4ce9a8e8084aff9aed391f5f0c9c228dfca52a2
Administrator <Administrator@PC-20170910YWGY>
2019-03-27 15:10:34 +0800

    test

--- src/Example02.java
+++ src/Example02.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Example02 {
 public static void main(String[] args) throws InterruptedException, IOException {
 FileOutputStream fileOutputStream = new FileOutputStream("example.txt");
-String str="我的名字叫苏晓峰";
+String str="我的名字叫苏晓峰22222222222222";
 byte[] b = str.getBytes();
 for (int i = 0; i < b.length; i++) {
 	fileOutputStream.write(b[i]);;
